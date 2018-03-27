b32700bb97286e178cd700ec8798bc1dd7cded32
Adrien987 <adrien.bardes@dbmail.com>
2018-03-27 14:33:10 +0200

    Half of the proof

--- pigeon_hole.v
+++ pigeon_hole.v
@@ -9,10 +9,26 @@ Inductive repeats : list Type -> Prop :=
 Axiom TE : forall (P : Prop), P \/ ~P.
 
 Definition pigeon_hole : forall (l1 l2 : list Type) (x : Type), 
-           (In x l1 -> In x l2) /\ (length l1 < length l2) ->
-           repeats l1.
+           (In x l1 -> In x l2) /\ (length l2 < length l1) ->
+           repeats l1. 
 Proof.
   intros.
   destruct H.
-  
+  induction l1.
+  - easy. 
+  - destruct (TE (In a l1)).
+    + constructor 1.
+      easy.
+    + constructor 2.
+      destruct (TE (x=a)).
+      * apply IHl1.
+        rewrite H2.
+        intro.
+        rewrite H2 in H.
+        (* pose proof (H (List.in_eq a l1)). *)
+        apply H.
+        apply List.in_eq.
+        (* assert (In a (a :: l1)).
+        apply List.in_eq. *) (* Deux autres façons de faire *)
+        
 Qed.
