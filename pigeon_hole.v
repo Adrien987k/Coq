bc35b10c27f630bcb9b33c46ee934928f9931d7f
Adrien987 <adrien.bardes@dbmail.com>
2018-04-03 14:32:07 +0200

    update

--- pigeon_hole.v
+++ pigeon_hole.v
@@ -8,20 +8,26 @@ Inductive repeats : list Type -> Prop :=
 
 Axiom TE : forall (P : Prop), P \/ ~P.
 
+Lemma aux : forall (x a : Type) l1 l2, x <> a -> In x (l1 ++ (a :: l2)) -> In x (l1 ++ l2).
+Proof.
+
+Admitted.
+
 Definition pigeon_hole : forall (l1 l2 : list Type) (x : Type), 
            (In x l1 -> In x l2) /\ (length l2 < length l1) ->
            repeats l1. 
 Proof.
-  intros.
-  destruct H.
+  intro.
   induction l1.
   - easy. 
   - destruct (TE (In a l1)).
     + constructor 1.
       easy.
     + constructor 2.
+      destruct H0.
+
       destruct (TE (x=a)).
-      * apply IHl1.
+      * subst.
         rewrite H2.
         intro.
         rewrite H2 in H.
@@ -30,5 +36,27 @@ Proof.
         apply List.in_eq.
         (* assert (In a (a :: l1)).
         apply List.in_eq. *) (* Deux autres façons de faire *)
-        
+        subst.
+      * 
 Qed.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
